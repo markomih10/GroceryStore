38b34a7c72bb9cc847206db832299d7f51ed8955
markomih10 <mmihajlovic@csuchico.edu>
2019-10-07 17:04:46 -0700

    Grocery Store List Counter

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Author: Marko Mihajlovic
 #include<iostream>
 #include<string>
 using namespace std;
@@ -7,7 +8,9 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-
+  string item;
+ do 
+  {
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
@@ -15,5 +18,22 @@ int main()
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
 
+  if(input == 'a' || input == 'A')
+  cout<<"What is the item?";
+  cin>>item;
+  
+  if (numItems <= 5)
+  {
+  list[numItems] = item;
+  
+  numItems++;
+  }
+  if(numItems > 5)
+ {
+  cout<<"You'll need a bigger list!";
+ }
+}
+ while(input != 'Q' && input != 'q');
+ 
   return 0;
 }
