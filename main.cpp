ae154ed3afc70ef54276784b702d227dd0ea35b7
markomih10 <mmihajlovic@csuchico.edu>
2019-10-07 17:20:52 -0700

    Grocery Store List Counter

--- main.cpp
+++ main.cpp
@@ -5,35 +5,35 @@ using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
-  string item;
- do 
-  {
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
+        string list[5]; //array of 5 strings
+        int numItems = 0;
+        char input;
+        string item;
+        do 
+        {
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
 
-  if(input == 'a' || input == 'A')
-  cout<<"What is the item?";
-  cin>>item;
-  
-  if (numItems <= 4)
-  {
-  list[numItems] = item;
-  
-  numItems++;
-  }
-  if(numItems > 4)
- {
-  cout<<"You'll need a bigger list!";
- }
-}
- while(input != 'Q' && input != 'q');
- 
-  return 0;
+                if(input == 'a' || input == 'A'){
+                        cout<<"What is the item?\n";
+                        cin>>item;
+
+                if (numItems <= 4){
+                
+                        list[numItems] = item;
+
+                        numItems++;
+                }
+                if(numItems > 4)
+                {
+                        cout<<"You'll need a bigger list!";
+                }
+        }}
+        while(input != 'Q' && input != 'q');
+
+        return 0;
 }
