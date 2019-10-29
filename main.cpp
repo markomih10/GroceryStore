8fe33d08059a35f151b94d56a80b2125e0c61232
markomih10 <mmihajlovic@csuchico.edu>
2019-10-28 23:54:31 -0700

    Newest

--- main.cpp
+++ main.cpp
@@ -10,31 +10,51 @@ int main()
         char input;
         string item;
         int counter=0;
+        int deleteTracker;
         do 
         {
                 cout<<"\n==GROCERY LIST MANAGER==";
                 cout<<"\nEnter your choice: ";
                 cout<<"\n (A)dd an item";
+                if (list.size() > 0)
+                {
+                    cout<<"\n (D)elete last item";
+                }
+                for(int i = 0; i < list.size(); i++)
+                { deleteTracker = list.size();
+
+                }
                 cout<<"\n (Q)uit";
                 cout<<"\nYour choice (A/Q): ";
                 cin>>input;
 
-                if(input == 'a' || input == 'A'){
+                if(input == 'a' || input == 'A')
+                {
                         cout<<"What is the item?\n";
                         cin>>item;
                         list.push_back (item);
 
 
                 }
+                if(input == 'D' || input == 'd')
+                {
+                        if( list.size() >0)
+                        {
+                                cout<<list.at(deleteTracker-1)<<" was deleted from the list."<<endl;
+                                list.pop_back();
+                        }
+                
+
         }
         while(input != 'Q' && input != 'q');
         if(list.size() == 0){
                 cout<<"No items to buy!"<<endl; }
-        else {
+        else 
+        {
                 cout<<"==ITEMS TO BUY=="<<endl;
                 for(int i=0; i < list.size(); i++) {
-                        cout<<counter+1<<" "<<list.at(i)<<endl;
+                        cout<<number+1<<" "<<list.at(i)<<endl;
+                        counter++;
                 }
-        }
-        return 0;
-}
+        } return 0;
+   }  
